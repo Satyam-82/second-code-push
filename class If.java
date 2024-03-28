421a8d9fcd6f20ac11d97b7750ef52bc837f67b8
Satyam-82 <151964870+Satyam-82@users.noreply.github.com>
2024-03-28 10:03:14 +0530

    update

---
+++ class If.java
@@ -0,0 +1,17 @@
+class If
+{
+    public static void main(String args[])
+    {
+        int x=0;
+        if
+        {
+           (x<=10);
+            System.out.println("hello");
+        }
+        else
+        {
+            (x>=10);
+            System.out.println("hii");
+        }
+    }
+}
